60fba8502353b2079d2712bffb4601e5db7f734c
MeirJC <meir84@gmail.com>
2024-01-29 21:12:47 +0200

    package manager replaced to npm

---
+++ .prettierrc.mjs
@@ -0,0 +1,15 @@
+// Official Prettier plugin adding support for formatting .astro files.
+// github.com/withastro/prettier-plugin-astro
+// .prettierrc.mjs
+/** @type {import("prettier").Config} */
+export default {
+  plugins: ["prettier-plugin-astro"],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+};
